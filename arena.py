9b4ae818e83c48b59840307c01e06389c9f0dffd
Richard Dunne <richard.w.dunne@gmail.com>
2016-01-28 00:30:05 +0000

    Added combat selectors and location dict

--- arena.py
+++ arena.py
@@ -13,6 +13,7 @@ class Enemy_win(object):
         len_x = w
         win = curses.newwin(len_y,len_x,starty,startx)
         win.border('|','|','-','-','+','+','+','+')
+        #win.addstr(1,1,str(win.getmaxyx))
         win.refresh()
 
 class Player_win(object):
@@ -24,15 +25,31 @@ class Player_win(object):
         len_x = w
         win = curses.newwin(len_y,len_x,starty,startx)
         win.border('|','|','-','-','+','+','+','+')
+        #win.addstr(1,1,str(win.getmaxyx))
         win.refresh()
 
 class Status_win(object):
     """Initialise a new status window with predetermined
         positions"""
     def __init__(self,h,w):
+        self.textpos = {}
+        self.actions = ['Attack','Defend','Special','Heal']
         starty, startx = int(round((h/5) * 4)), 0
         len_y = int(round(h/5))
         len_x = w
         win = curses.newwin(len_y,len_x,starty,startx)
         win.border('|','|','-','-','+','+','+','+')
+        #win.addstr(1,1,str(win.getmaxyx))
+
+        for i in range(4):
+            padding = ((((len_x/4)*i) / len(self.actions[i])) + 24) #hack to avoid 0'th' index returning 0\
+                    #this needs to be changed when factoring in different sized terminals.  TODO
+            self.textpos.update({self.actions[i]:((len_y/2)+1,(((len_x/4)*i) + (padding/2))+2)})
+            if padding <= 1:
+                win.addstr(len_y/2,((len_x/4)*i) + (padding/2), self.actions[i][:2])
+            else:
+                win.addstr(len_y/2,((len_x/4)*i) + (padding/2), self.actions[i]) #ugly, but ok for now
+            win.addch((len_y/2)+1,(((len_x/4)*i) + (padding/2))+2, ord('^'))
+            win.addstr(2,1,str(self.textpos))
         win.refresh()
+
